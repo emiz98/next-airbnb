4b6e06ecbaf29a710fad3c7a9f93d8bc0da51395
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-04-02 20:02:54 +0530

    Added Calendar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,43 +1,129 @@
-import React from "react";
-import { Language, Menu, Search } from "@mui/icons-material";
+import React, { useState } from 'react'
+import { Close, Group, Language, Menu, Search } from '@mui/icons-material'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+import { DateRangePicker } from 'react-date-range'
+import { useRouter } from 'next/router'
 
-const Header = () => {
+const Header = ({ placeholder }) => {
+  const router = useRouter()
+  const [searchInput, setSearchInput] = useState('')
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+  const [noOfGuests, setNoOfGuests] = useState(1)
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  }
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+  }
+
   return (
-    <header className="bg-white shadow-md p-3 md:p-5 md:px-10 sticky top-0 z-50 flex justify-between items-center">
-      <img
-        className="w-28 md:w-32 object-contain cursor-pointer"
-        src="/logo.png"
-        alt="logo"
-      />
-      <div className="hidden md:flex lg:flex-grow max-w-2xl rounded-full items-center md:shadow-sm md:border-2 py-2">
-        <input
-          className="pl-5 bg-transparent text-gray-600 md:mr-2 outline-none flex-grow"
-          type="text"
-          placeholder="Start your search"
+    <header className="sticky top-0 z-50 bg-white p-3 shadow-md md:p-5 md:px-10">
+      <div className="flex items-center justify-between">
+        <img
+          onClick={() => router.push('/')}
+          className="w-28 cursor-pointer object-contain md:w-32"
+          src="/logo.png"
+          alt="logo"
         />
-        <span className="hidden md:flex cursor-pointer md:mr-2 items-center rounded-full p-2 bg-red-400">
-          <Search className=" text-white" />
-        </span>
-      </div>
-      <div className="flex items-center space-x-5 font-medium text-gray-500">
-        <span className="hidden md:inline-flex cursor-pointer hover:underline">
-          Become a host
-        </span>
-        <Language className="hidden md:inline-flex" />
-        <div
-          className="flex items-center space-x-3 border md:border-2 border-gray-300
-        shadow-sm p-1 md:p-2 rounded-full"
-        >
-          <Menu className="ml-2 ease-out cursor-pointer hover:scale-110 transition" />
-          <img
-            className="w-10 cursor-pointer hover:scale-105 transition ease-out rounded-full"
-            src="/avatar.png"
-            alt="avatar"
+        <div className="hidden max-w-2xl items-center rounded-full py-2 md:flex md:border-2 md:shadow-sm lg:flex-grow">
+          <input
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            className="flex-grow bg-transparent pl-5 text-gray-600 outline-none md:mr-2"
+            type="text"
+            placeholder={placeholder || 'Start your search'}
           />
+          {searchInput && (
+            <span
+              onClick={() => setSearchInput('')}
+              className="mr-3 flex cursor-pointer items-center"
+            >
+              <Close className="text-lg text-gray-600" />
+            </span>
+          )}
+          <span
+            className="hidden cursor-pointer items-center rounded-full bg-red-400 p-2 transition ease-in-out
+          hover:bg-red-500 md:mr-2 md:flex"
+          >
+            <Search className=" text-white" />
+          </span>
+        </div>
+        <div className="flex items-center space-x-5 font-medium text-gray-500">
+          <span className="hidden cursor-pointer hover:underline lg:inline-flex">
+            Become a host
+          </span>
+          <Language className="hidden md:inline-flex" />
+          <div
+            className="flex items-center space-x-3 rounded-full border border-gray-300
+        p-1 shadow-sm md:border-2 md:p-2"
+          >
+            <Menu className="ml-2 cursor-pointer transition ease-out hover:scale-110" />
+            <img
+              className="w-10 cursor-pointer rounded-full transition ease-out hover:scale-105"
+              src="/avatar.png"
+              alt="avatar"
+            />
+          </div>
         </div>
       </div>
-    </header>
-  );
-};
 
-export default Header;
+      {searchInput && (
+        <div className="mt-5 flex flex-col items-center lg:mr-48">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+          <div className="mx-10 mb-4 flex w-full items-center border-b md:w-7/12 lg:w-1/3">
+            <h2 className="flex-grow text-2xl font-medium">Number of Guests</h2>
+            <Group />
+            <input
+              min={1}
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              className="w-12 pl-2 text-lg text-red-400 outline-none"
+              type="number"
+            />
+          </div>
+          <div className="mt-5 flex w-1/2 items-center gap-x-5">
+            <button
+              onClick={() => setSearchInput('')}
+              className="flex-grow cursor-pointer rounded-lg bg-gray-400 px-5 
+            py-3 font-medium text-white transition ease-in-out hover:bg-gray-500"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={() => {
+                router.push({
+                  pathname: '/search',
+                  query: {
+                    location: searchInput,
+                    startDate: startDate.toDateString(),
+                    endDate: endDate.toDateString(),
+                    noOfGuests,
+                  },
+                }),
+                  setSearchInput('')
+              }}
+              className="flex-grow cursor-pointer rounded-lg bg-red-400 px-5 
+            py-3 font-medium text-white transition ease-in-out hover:bg-red-500"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      )}
+    </header>
+  )
+}
+
+export default Header
---
+++ pages/search.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { format } from 'date-fns'
+
+const Search = () => {
+  const router = useRouter()
+  const { location, startDate, endDate, noOfGuests } = router.query
+
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yy')
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yy')
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+
+  console.log(router.query)
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb 1.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+      <main className="flex">
+        <section className="flex-grow p-3 pt-14 md:p-5 md:px-10">
+          <p className="text-xs">
+            300+ Stays - {range} for {noOfGuests} guests
+          </p>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">
+            Stays in {location}
+          </h1>
+          <div className="mb-3 hidden select-none items-center space-x-3 whitespace-nowrap text-gray-800 md:flex">
+            <button className="sortBtn">Cancellation Flexibility</button>
+            <button className="sortBtn">Type of Place</button>
+            <button className="sortBtn">Price</button>
+            <button className="sortBtn">Rooms and Beds</button>
+            <button className="sortBtn">More Filters</button>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Search
